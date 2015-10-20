b25a4c9001890d39b6c26121bf762cceebdd6ea5
Logan Short <lshort@stanford.edu>
2015-10-20 01:01:43 -0700

    Added support for blocks of dead pieces

--- board.py
+++ board.py
@@ -18,18 +18,22 @@ class Board:
     # Sets cursor to the defualt position which is in the middle of the board
     # If input rows is specified then creates a board with the supplied pieces
     # If input board is specified then creates a copy of the board
-    def __init__(self, inputRows=None, inputBoard=None):
+    def __init__(self, inputRows=None, inputDeadBlocks=None, inputBoard=None):
         self.rows = []
+        self.deadBlocks = []
         if inputBoard != None:
             for r in xrange(HEIGHT):
                 row = list(inputBoard.rows[r])
                 self.rows.append(row)
             self.cursor = inputBoard.cursor
+            self.deadBlocks = list(inputBoard.deadBlocks)
         elif inputRows != None:
             for r in xrange(HEIGHT):
                 row = list(inputRows[r])
                 self.rows.append(row)
             self.cursor = (5,2)
+            if inputDeadBlocks != None:
+                self.deadBlocks = list(inputDeadBlocks)
         else:
             for r in xrange(HEIGHT):
                 row = []
@@ -45,27 +49,58 @@ class Board:
         for c in xrange(WIDTH):
             fallHeight.append(0)
         for r in xrange(HEIGHT):
+            deadChunks = set()
             for c in xrange(WIDTH):
                 if FIRE <= self.rows[r][c].getValue():
-                    if fallHeight[c] < r:
+                    if fallHeight[c] < r and self.rows[r][c].getValue() <= POKEBALL:
                         self.rows[fallHeight[c]][c].setValue(self.rows[r][c].getValue())
                         self.rows[r][c].setValue(EMPTY)
-                    fallHeight[c] += 1
+                    if self.rows[r][c].getValue() == DEAD:
+                        deadBlock = self.findDeadBlock((r,c))
+                        deadChunks.add(deadBlock)
+                    else:
+                        fallHeight[c] += 1
+            for chunk in deadChunks:
+                if chunk[0][0] != 0:
+                    peak = 0
+                    for c in xrange(chunk[0][1], chunk[1][1]):
+                        peak = max(peak, fallHeight[c])
+                    if peak < chunk[0][0]:
+                        for c in xrange(chunk[0][1], chunk[1][1]+1):
+                            fallHeight[c] = peak+1
+                            for r in xrange(chunk[0][0], chunk[1][0]+1):
+                                self.rows[peak+(r-chunk[0][0])][c].setValue(DEAD)
+                                self.rows[r][c].setValue(EMPTY)
+
+    def findDeadBlock(self, deadPiece):
+        for block in self.deadBlocks:
+            if block[0][0] <= deadPiece[0] and block[0][1] <= deadPiece[1] \
+               and block[1][0] >= deadPiece[0] and block[1][1] >= deadPiece[1]:
+                return block
+        return None
 
     # Sets a board's pieces to match those specified by inputRows
-    def setFromRows(self, inputRows):
+    def setFromRows(self, inputRows, inputDeadBlocks=[]):
         for r in xrange(HEIGHT):
             for c in xrange(WIDTH):
-                self.rows[r][c] = inputRows[r][c]        
+                self.rows[r][c] = inputRows[r][c]
+        self.deadBlocks = list(inputDeadBlocks)
 
     # Sets a board's pieces to match a board described by an input file
     def loadFromFile(self, inputFile):
         with open(inputFile) as f:
             inputBoard = f.readlines()
-        for line in enumerate(inputBoard):
-            values = line[1].split()
+        for l in xrange(HEIGHT):
+            values = inputBoard[l].split()
             for v in xrange(len(values)):
-                self.rows[HEIGHT-(line[0]+1)][v].setValue(int(values[v]))
+                self.rows[HEIGHT-(l+1)][v].setValue(int(values[v]))
+        self.deadBlocks = []
+        for l in xrange(HEIGHT,len(inputBoard)):
+            deadBlock = inputBoard[l].split("|")
+            firstCorner = deadBlock[0].split(",")
+            secondCorner = deadBlock[1].split(",")
+            self.deadBlocks.append(((int(firstCorner[0]), int(firstCorner[1])),\
+                                    (int(secondCorner[0]),int(secondCorner[1]))))
 
     # Prints out a board
     def printBoard(self):
@@ -78,8 +113,8 @@ class Board:
 
 """
 board = Board()
-board.loadFromFile('TestBoards/board2.txt')
+board.loadFromFile('TestBoards/board3.txt')
 board.printBoard()
 board.applyGravity()
 board.printBoard()
-"""  
+"""
